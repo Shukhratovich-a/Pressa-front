94f312bf7e5ca3f2ac03eeca79a75bb446510745
Shukhratovich-a <shukhratovich138@gmail.com>
2022-07-28 02:46:15 +0500

    Add benefits section to home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Hero from "../../Components/Hero/Hero";
 import LastPosts from "../../Components/LastPosts/LastPosts";
+import Benefits from "../../Components/Benefits/Benefits";
 
 const Home = () => {
   return (
     <main className="mian">
       <Hero />
       <LastPosts />
+      <Benefits />
     </main>
   );
 };

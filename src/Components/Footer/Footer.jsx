0466b857278b1511c4717f7683c5b5ddfb12bad8
Shukhratovich-a <shukhratovich138@gmail.com>
2022-07-28 02:59:30 +0500

    Add link and nav to footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,19 @@
+import { Link } from "react-router-dom";
+
+import Nav from "../Nav/Nav";
+
+import styles from "./Footer.module.scss";
+
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="container">footer</div>
+    <footer className={styles.footer}>
+      <div className={`container ${styles.container}`}>
+        <Link className={styles.footer__link} to="/">
+          Pressa
+        </Link>
+
+        <Nav style={styles.footer__nav__item} />
+      </div>
     </footer>
   );
 };

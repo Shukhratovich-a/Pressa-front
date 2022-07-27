6c3f380b772e5de94ece106aed431930dbaac23c
Shukhratovich-a <shukhratovich138@gmail.com>
2022-07-28 02:52:51 +0500

    Finish benefits section

--- src/Components/Benefits/Benefits.jsx
+++ src/Components/Benefits/Benefits.jsx
@@ -1,3 +1,6 @@
+import BenefitFirst from "../../Assets/Images/BenefitFirst.png";
+import BenefitSecond from "../../Assets/Images/BenefitSecond.png";
+
 import styles from "./Benefits.module.scss";
 
 const Benefits = () => {
@@ -6,10 +9,26 @@ const Benefits = () => {
       <div className={`container`}>
         <ul className={styles.benefits__list}>
           <li className={styles.benefit}>
+            <img
+              className={styles.benefit__image}
+              src={BenefitFirst}
+              alt=""
+              width={300}
+              height={377}
+            />
+
             <h3 className={styles.benefit__heading}>E’LONLARINGIZNI BIZNI SAYTDA BERING</h3>
           </li>
 
           <li className={styles.benefit}>
+            <img
+              className={styles.benefit__image}
+              src={BenefitSecond}
+              alt=""
+              width={300}
+              height={393}
+            />
+
             <h3 className={styles.benefit__heading}>ENG SO’NGI MASTER KLASLAR BIZNING SAYTDA</h3>
           </li>
         </ul>

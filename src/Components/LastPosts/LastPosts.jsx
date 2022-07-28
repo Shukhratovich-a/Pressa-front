a3edcc4658e1a2457b3d47a8458f4fd5859136ab
Shukhratovich-a <shukhratovich138@gmail.com>
2022-07-28 18:53:20 +0500

    Connect post component

--- src/Components/LastPosts/LastPosts.jsx
+++ src/Components/LastPosts/LastPosts.jsx
@@ -1,6 +1,4 @@
-import User from "../Lib/Icons/User";
-import Calendar from "../Lib/Icons/Calendar";
-import Type from "../Lib/Icons/Type";
+import Post from "../Post/Post";
 
 import styles from "./LastPosts.module.scss";
 
@@ -12,46 +10,8 @@ const LastPosts = () => {
         <h2 className={styles["last-posts__heading"]}>Oxirgi e’lonlar</h2>
 
         <ul className={styles["last-posts__list"]}>
-          {array.map((item) => (
-            <li className={styles.post} key={item}>
-              <img
-                className={styles.post__image}
-                src={"https://via.placeholder.com/407x417/006aff"}
-                alt=""
-                width={407}
-                height={417}
-              />
-
-              <div className={styles.post__inner}>
-                <h3 className={styles.post__heading}>
-                  Alisher Isaevdan biznes va IT bo’yicha master klass
-                </h3>
-
-                <ul className={styles.post__list}>
-                  <li className={styles.post__item}>
-                    <User />
-                    <span>Alisher Isaev</span>
-                  </li>
-                  <li className={styles.post__item}>
-                    <span>Tadbirkor</span>
-                  </li>
-                  <li className={styles.post__item}>
-                    <Calendar />
-                    <span>17 / 01 / 2022</span>
-                  </li>
-                  <li className={styles.post__item}>
-                    <span>15:00</span>
-                  </li>
-                  <li className={styles.post__item}>
-                    <Type />
-                    <span>Online</span>
-                  </li>
-                  <li className={styles.post__item}>
-                    <span>2550</span>
-                  </li>
-                </ul>
-              </div>
-            </li>
+          {array.map((post) => (
+            <Post post={post} key={post}/>
           ))}
         </ul>
 

27d5a4d69dcc32cc865330069750f36171953799
Shukhratovich-a <shukhratovich138@gmail.com>
2022-07-28 18:50:01 +0500

    Create types component

---
+++ src/Components/Types/Types.jsx
@@ -0,0 +1,44 @@
+import useType from "../../Hooks/useType";
+
+import Radio from "../Lib/Inputs/Radio/Radio";
+
+import Online from "../Lib/Icons/Online";
+import Offline from "../Lib/Icons/Offline";
+
+import styles from "./Types.module.scss";
+
+const Type = () => {
+  const [type, setType] = useType();
+
+  const types = [
+    { title: "online", icon: Online },
+    { title: "offline", icon: Offline },
+  ];
+
+  const handleCheck = (evt) => {
+    if (evt.target.checked) setType(evt.target.value);
+  };
+
+  return (
+    <div className={styles.types}>
+      <ul className={styles.types__list}>
+        {types.length > 0 &&
+          types.map((item) => (
+            <li className={styles.type} key={item.title}>
+              <label className={styles.type__label}>
+                <item.icon />
+                <span className={styles.type__text}>{item.title}</span>
+                <Radio
+                  value={item.title}
+                  isChecked={item.title === type ? true : false}
+                  onChange={handleCheck}
+                />
+              </label>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Type;

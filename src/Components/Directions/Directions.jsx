e61eb55183e8b9d94a4ae28da6d4fd18bcd31a89
Shukhratovich-a <shukhratovich138@gmail.com>
2022-07-28 18:42:29 +0500

    Add loading and custom inputs

--- src/Components/Directions/Directions.jsx
+++ src/Components/Directions/Directions.jsx
@@ -2,12 +2,19 @@ import React from "react";
 
 import useDirections from "../../Hooks/useDirections";
 
-import Tick from "../Lib/Icons/Tick";
+import CheckBox from "../Lib/Inputs/CheckBox/CheckBox";
+
+import Loading from "../Lib/Loading/Loading";
 
 import styles from "./Directions.module.scss";
 
 const Directions = () => {
   const [directions, setDirections] = useDirections();
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    if (directions.length) setLoading(false);
+  }, [directions]);
 
   const handleCheck = (evt, value, subValue) => {
     const array = [...directions];
@@ -23,34 +30,33 @@ const Directions = () => {
   };
 
   return (
-    <div className={styles.directions}>
-      <ul className={styles.directions__list}>
-        {directions.map((direction) => (
-          <li className={styles.direction} key={direction.title}>
-            <h3 className={styles.direction__heading}>{direction.title}</h3>
+    <div className={`${styles.directions} ${loading ? styles["directions--loading"] : ""}`}>
+      <ul className={`${styles.directions__list}`}>
+        {directions.length > 0 &&
+          directions.map((direction) => (
+            <li className={styles.direction} key={direction.title}>
+              <h3 className={styles.direction__heading}>{direction.title}</h3>
 
-            <ul className={styles.direction__sublist}>
-              {direction.subDirection.map((subDirection) => (
-                <li className={styles.subdirection} key={subDirection.title}>
-                  <label className={styles.subdirection__label}>
-                    <input
-                      className={`${styles.subdirection__input} visually-hidden`}
-                      type={"checkbox"}
-                      defaultValue={subDirection.title}
-                      defaultChecked={subDirection.isChecked}
-                      onChange={(evt) => handleCheck(evt, direction.title, subDirection.title)}
-                    />
-                    <span className={styles["subdirection__input--controller"]}>
-                      <Tick />
-                    </span>
-                    <h4 className={styles.subdirection__title}>{subDirection.title}</h4>
-                  </label>
-                </li>
-              ))}
-            </ul>
-          </li>
-        ))}
+              <ul className={`${styles.direction__sublist}`}>
+                {direction.subDirection.length > 0 &&
+                  direction.subDirection.map((subDirection) => (
+                    <li className={styles.subdirection} key={subDirection.title}>
+                      <label className={styles.subdirection__label}>
+                        <CheckBox
+                          value={subDirection.title}
+                          isChecked={subDirection.isChecked}
+                          onChange={(evt) => handleCheck(evt, direction.title, subDirection.title)}
+                        />
+                        <h4 className={styles.subdirection__title}>{subDirection.title}</h4>
+                      </label>
+                    </li>
+                  ))}
+              </ul>
+            </li>
+          ))}
       </ul>
+
+      {loading && <Loading />}
     </div>
   );
 };

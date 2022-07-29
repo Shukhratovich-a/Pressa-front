d275136d3292ef3788a7b103cb831474c237b658
Shukhratovich-a <shukhratovich138@gmail.com>
2022-07-30 01:10:17 +0500

    Create advertisment page

---
+++ src/Pages/Advertisement/Advertisement.jsx
@@ -0,0 +1,11 @@
+import styles from "./Advertisement.module.scss";
+
+const Advertisement = () => {
+  return (
+    <main className={styles.main}>
+      <div className="container">Advertisement</div>
+    </main>
+  );
+};
+
+export default Advertisement;

7fb261be785968d5d567f045641a833a0e3c4661
Shukhratovich-a <shukhratovich138@gmail.com>
2022-07-30 01:14:09 +0500

    Edit post

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,19 +1,24 @@
+import useDate from "../../Hooks/useDate";
+
 import User from "../Lib/Icons/User";
 import Calendar from "../Lib/Icons/Calendar";
 import Online from "../Lib/Icons/Online";
+import Offline from "../Lib/Icons/Offline";
 
 import styles from "./Post.module.scss";
 
 const Post = ({ post }) => {
   return (
     <li className={styles.post}>
-      <img
-        className={styles.post__image}
-        src={"https://via.placeholder.com/407x417/006aff"}
-        alt=""
-        width={407}
-        height={417}
-      />
+      <div className={styles.post__wrapper}>
+        <img
+          className={styles.post__image}
+          src={post.post.post_image_link}
+          alt=""
+          width={407}
+          height={417}
+        />
+      </div>
 
       <div className={styles.post__inner}>
         <h3 className={styles.post__heading}>
@@ -23,26 +28,23 @@ const Post = ({ post }) => {
         <ul className={styles.post__list}>
           <li className={styles.post__item}>
             <User />
-            <span>Alisher Isaev</span>
+            <span>{post.organizer.organizer_name}</span>
           </li>
           <li className={styles.post__item}>
-            
-            <span>Tadbirkor</span>
+            <span>{post.organizer.organizer_profession}</span>
           </li>
           <li className={styles.post__item}>
             <Calendar />
-            <span>17 / 01 / 2022</span>
+            <span>{useDate(post.conference_date)}</span>
           </li>
           <li className={styles.post__item}>
-            
-            <span>15:00</span>
+            <span>{useDate(post.conference_date, "time")}</span>
           </li>
           <li className={styles.post__item}>
-            <Online />
-            <span>Online</span>
+            {post.conference_type === "online" ? <Online /> : <Offline />}
+            <span>{post.conference_type}</span>
           </li>
           <li className={styles.post__item}>
-            
             <span>2550</span>
           </li>
         </ul>
